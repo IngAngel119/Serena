ec92f95cace22627a092ccf5f50bec8b3f3fc35d
IngAngel119 <aescalantegarza@gmail.com>
2025-03-28 11:59:06 -0600

    Initial Commit

---
+++ Serena-Backend/Serena_BackendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Serena_BackendApp.swift
+//  Serena-Backend
+//
+//  Created by Angel Escalante Garza on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Serena_BackendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Serena-BackendTests/Serena_BackendTests.swift
@@ -0,0 +1,17 @@
+//
+//  Serena_BackendTests.swift
+//  Serena-BackendTests
+//
+//  Created by Angel Escalante Garza on 28/03/25.
+//
+
+import Testing
+@testable import Serena_Backend
+
+struct Serena_BackendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
